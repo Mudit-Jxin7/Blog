2df23f0971e751f513085e34ee11f96d2210814c
Mudit <muditert34@gmail.com>
2023-05-31 16:11:50 +0530

    Backend established

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,14 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form action="" className="register">
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return (
+    <div>
+      <form className="register" onSubmit={register}>
+        <h1>Register</h1>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
