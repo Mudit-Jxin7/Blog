a90be63fddfc8fa67bde21c7672ba1e87de8e807
Mudit <muditert34@gmail.com>
2023-06-01 22:04:35 +0530

    updated login and implemented basic logout

--- server/index.js
+++ server/index.js
@@ -5,12 +5,18 @@ const mongoose = require('mongoose');
 const User = require('./models/User')
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "hcewqyt378w35thgnbw";
 
-app.use(cors({ credentials: true, origin: 'http://127.0.0.1:5173' }))
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173'
+}));
 app.use(express.json())
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://muditert34:PoU9KJxkXBmt0k6O@cluster0.qrlonzo.mongodb.net/?retryWrites=true&w=majority')
 
@@ -34,12 +40,28 @@ app.post('/login', async (req, res) => {
     if (passOk) {
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err
-            res.cookie('token', token).json('ok')
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            }).json('ok');
+
         })
     } else {
         res.status(400).json('wrong credentials')
     }
 })
 
-app.listen(4000);
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+})
 
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok')
+})
+
+app.listen(4000);
